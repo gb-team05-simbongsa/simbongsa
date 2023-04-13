2bf50b53adbffa9467290d08618dfe09b6ee2f67
code-hyun <jh940412@gmail.com>
2023-04-13 13:30:38 +0900

    start

---
+++ src/main/java/com/app/simbongsa/SimbongsaApplication.java
@@ -0,0 +1,13 @@
+package com.app.simbongsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbongsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimbongsaApplication.class, args);
+    }
+
+}
