1f73c96d3b55304ec240b45070cc0ac5966c75a1
code-hyun <jh940412@gmail.com>
2023-04-15 00:36:56 +0900

    admin 퍼블리싱

---
+++ src/main/resources/static/js/admin/app.js
@@ -0,0 +1,39 @@
+/**
+ * ==================================================================================================
+ *   DOM
+ *   ==================================================================================================
+ *
+ * @format
+ */
+
+/* 사이드바 */
+const $sidebar = $('.sidebar__main');
+const $sidebarSlide = $('.sidebar__wrapper__slider');
+
+/* 테이블 내용(컨텐츠) */
+const $tableContent = $('.table__content');
+
+/* 체크박스 */
+const $checkAll = $('#checkAll');
+const $check = $("input[name='check']");
+
+/* 검색조건 select-box */
+const $selectBox = $('.listbox-selecter');
+const $listbox = $('.listbox');
+const $list = $('.list');
+const $searchType = $("input[name='searchType']");
+
+/* 검색바 */
+const $searchBar = $('.search-input');
+
+/* 상세보기 관련 */
+const $detailButton = $('.content__detail__btn');
+const $modalStage = $('.modal-stage');
+
+/* 시게 */
+const clock = document.querySelector('.clock h1');
+
+/* 모달창 */
+/* 파일 썸네일교체 */
+const $storageFile = $("input[name='file']");
+const $thumbnail = $(".content__img img");
