6a8677abea00f6d59e74bd4458f556b9a8c8128b
coder-juyeon <test.2678ju@gmail.com>
2023-04-27 02:25:22 +0900

    후원 상세페이지 일단 클론코딩까지 완료

---
+++ src/main/resources/static/js/service-info/serviceInfo.js
@@ -0,0 +1,17 @@
+/* var timeout = null;
+var count = 0;
+
+$(document).on('scroll', function(){
+    count++
+    $(window).scrollTop(937 * count);
+  }); */
+
+/* $(document).on('scroll', function(){
+    clearTimeout(timeout);
+    timeout = setTimeout(function(){
+    var translateY = count++ * -937;
+    console.log(translateY);
+    $('.slide-wrapper').css('transform', 'translate3d(0px,' + translateY + 'px, 0px)');
+    $('.slide-wrapper').css('transition-duration', '550ms');
+    }, 200);
+  }); */
