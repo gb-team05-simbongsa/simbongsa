58e473ae1ede25d9654906c17fdab4db049c64d6
coding-moon <dawn43214@gmail.com>
2023-05-02 22:56:18 +0900

    모달창 js  입히기 완료

---
+++ src/main/resources/static/js/volunteer-work/work-apply-modal.js
@@ -0,0 +1,22 @@
+const $open = $('#work-open');
+const $layout = $('.apply-layout');
+const $close = $('.apply-close');
+
+$(document).ready(function(){
+
+// 버튼 클릭시 모달창 띄우기 
+$open.click(function(){
+    $layout.show();
+});
+
+// x버튼 클릭시 모달창 닫기
+$close.click(function(){
+    $layout.hide();
+})
+
+// 전송 
+// $submit.click(function(){
+//     document.$submit();
+// });
+
+});
