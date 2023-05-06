3c13359d6452dc634e2a08c9ae246243bcc5d1fb
code-hyun <jh940412@gmail.com>
2023-05-07 00:51:40 +0900

    freeBoardRepositoryTests, VolunteerRepositoryTests 생성

---
+++ src/test/java/com/app/simbongsa/repository/board/FreeBoardRepositoryTests.java
@@ -0,0 +1,14 @@
+package com.app.simbongsa.repository.board;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+@Slf4j
+public class FreeBoardRepositoryTests {
+}
---
+++ src/test/java/com/app/simbongsa/repository/volunteer/VolunteerRepositoryTests.java
@@ -0,0 +1,24 @@
+package com.app.simbongsa.repository.volunteer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+@Slf4j
+public class VolunteerRepositoryTests {
+//    VolunteerWorkRepository volunteerWorkRepository;
+//
+//    @Test
+//    public void saveTest(){
+//        for(int i = 0; i < 50; i++){
+//
+//        }
+//    }
+
+}
