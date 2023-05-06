0728b35f81b617e644b3d7b40ae43b81b3dd1e47
coder-juyeon <test.2678ju@gmail.com>
2023-05-06 19:42:41 +0900

    후원요청목록조회(byUserid)

--- src/main/java/com/app/simbongsa/repository/support/SupportRequestQueryDsl.java
+++ src/main/java/com/app/simbongsa/repository/support/SupportRequestQueryDsl.java
@@ -1,4 +1,10 @@
 package com.app.simbongsa.repository.support;
 
+import com.app.simbongsa.entity.support.SupportRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface SupportRequestQueryDsl {
+    /* 유저별 후원 요청 조회 */
+    public Page<SupportRequest> findByUserId(Pageable pageable, Long id);
 }
