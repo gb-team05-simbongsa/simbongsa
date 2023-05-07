94fff1abf3eb5f39944a7f403f2deac0fe468b97
code-hyun <jh940412@gmail.com>
2023-05-07 19:52:23 +0900

    커밋

--- src/test/java/com/app/simbongsa/repository/user/UserRepositoryTests.java
+++ src/test/java/com/app/simbongsa/repository/user/UserRepositoryTests.java
@@ -68,7 +68,7 @@ public class UserRepositoryTests {
 //    유저 공양미 조회(후원 상세페이지 공양미 조회용)
     @Test
     public void findUserPaymentById(){
-        Optional<User> goyangmi = userRepository.findById(110L);
-        goyangmi.ifPresent(user -> log.info(user.getUserRice()+ ""));
+        Optional<User> goyangmi = userRepository.findById(715L);
+        goyangmi.ifPresent(user -> log.info("====================================" + user.getUserRice()+ "====================="));
     }
 }
