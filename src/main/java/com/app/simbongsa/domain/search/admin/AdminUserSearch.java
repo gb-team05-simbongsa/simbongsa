8bbb10e53553c580fcb076e1a839424cef91eb83
dev-hyunjin <disappointed123419@gmail.com>
2023-05-08 23:33:33 +0900

    관리자 회원, 문의 검색 작업중

---
+++ src/main/java/com/app/simbongsa/domain/search/admin/AdminUserSearch.java
@@ -0,0 +1,10 @@
+package com.app.simbongsa.domain.search.admin;
+
+import com.app.simbongsa.type.UserRankType;
+import lombok.Data;
+
+@Data
+public class AdminUserSearch {
+    private String userEmail;
+    private String userAddress;
+}
