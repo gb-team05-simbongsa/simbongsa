41e67f9bf1717a083afeadadc8da0da2b77ebe93
LFseohyeon <m9037m@gmail.com>
2023-05-12 11:44:30 +0900

    서현 자유게시판 서비스 임시커밋

--- src/main/java/com/app/simbongsa/service/board/FreeBoardService.java
+++ src/main/java/com/app/simbongsa/service/board/FreeBoardService.java
@@ -1,14 +1,24 @@
 package com.app.simbongsa.service.board;
 
+import com.app.simbongsa.domain.FreeBoardDTO;
 import com.app.simbongsa.entity.board.FreeBoard;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface FreeBoardService {
-//    최신순 무한스크롤 전체 목록
-    public Slice<FreeBoard> getAllNewFreeBoards(Pageable pageable);
+    // 저장
+    public void register(FreeBoardDTO freeBoardDTO, Long memberId);
 
-//    인기순 무한스크롤 전체 목록
-    public Slice<FreeBoard> getAllLikeFreeBoards(Pageable pageable);
+    // 최신순 목록
+    public Slice<FreeBoardDTO> getNewList(Pageable pageable);
+
+    // 인기순 목록
+    public Slice<FreeBoardDTO> getLikesList(Pageable pageable);
+
+    // 상세 보기
+    public FreeBoardDTO getDetail(Long memberId);
+
+    // 작성
+    public void write(FreeBoard freeBoard);
 
 }
--- src/main/java/com/app/simbongsa/service/board/FreeBoardServiceImpl.java
+++ src/main/java/com/app/simbongsa/service/board/FreeBoardServiceImpl.java
@@ -1,13 +1,19 @@
 package com.app.simbongsa.service.board;
 
+import com.app.simbongsa.domain.FreeBoardDTO;
 import com.app.simbongsa.entity.board.FreeBoard;
 import com.app.simbongsa.repository.board.FreeBoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,16 +21,35 @@ import org.springframework.stereotype.Service;
 public class FreeBoardServiceImpl implements FreeBoardService{
     private final FreeBoardRepository freeBoardRepository;
 
+    /*저장*/
+    @Override
+    public void register(FreeBoardDTO freeBoardDTO, Long memberId) {
+
+    }
+
     /*최신순 무한스크롤 전체 목록*/
     @Override
-    public Slice<FreeBoard> getAllNewFreeBoards(Pageable pageable) {
-        return freeBoardRepository.findAllByIdDescWithPaging_QueryDSL(pageable);
+    public Slice<FreeBoardDTO> getNewList(Pageable pageable) {
+        return null;
     }
 
     /*인기순 무한스크롤 전체 목록*/
     @Override
-    public Slice<FreeBoard> getAllLikeFreeBoards(Pageable pageable) {
-        return freeBoardRepository.findAllByLikeCountDescWithPaging_QueryDSL(pageable);
+    public Slice<FreeBoardDTO> getLikesList(Pageable pageable) {
+        return null;
     }
+
+    /*상세*/
+    @Override
+    public FreeBoardDTO getDetail(Long memberId) {
+        return null;
+    }
+
+    /*작성*/
+    @Override
+    public void write(FreeBoard freeBoard) {
+        freeBoardRepository.save(freeBoard);
+    }
+
 
 }
