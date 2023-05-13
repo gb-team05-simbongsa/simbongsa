c9cdc1aa440414f33d9cadab9963ae93a4cbf51c
coder-juyeon <test.2678ju@gmail.com>
2023-05-13 20:03:16 +0900

    내 문의사항 목록 조회, 문의답변 조회

--- src/main/java/com/app/simbongsa/service/inquiry/AnswerServiceImpl.java
+++ src/main/java/com/app/simbongsa/service/inquiry/AnswerServiceImpl.java
@@ -2,6 +2,7 @@ package com.app.simbongsa.service.inquiry;
 
 import com.app.simbongsa.domain.AnswerDTO;
 import com.app.simbongsa.repository.inquiry.AnswerRepository;
+import com.app.simbongsa.repository.inquiry.InquiryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -14,9 +15,17 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class AnswerServiceImpl implements AnswerService {
     private final AnswerRepository answerRepository;
+    private final InquiryRepository inquiryRepository;
 
     @Override
     public void saveAnswer(AnswerDTO answerDTO) {
         answerRepository.save(toAnswerEntity(answerDTO));
+    }
+
+    /* 질문에 대한 답변 조회*/
+    @Override
+    public AnswerDTO findByInquiryId(Long inquiryId) {
+        return toAnswerDTO(answerRepository.findByInquiryId_QueryDSL(inquiryId).get());
+
     }
 }
