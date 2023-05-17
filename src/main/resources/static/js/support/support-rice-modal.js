6e07f757ea143842dd4eb2baae7e07fc5af01134
code-hyun <jh940412@gmail.com>
2023-05-17 17:33:49 +0900

    후원 상세페이지 작업중

--- src/main/resources/static/js/support/support-rice-modal.js
+++ src/main/resources/static/js/support/support-rice-modal.js
@@ -1,102 +1,104 @@
 const $open = $('.donate');
-    const $layout = $('.support-layout');
+const $layout = $('.support-layout');
     const $submit = $('.support-pay-layout');
     const $close = $('.support-rice-close-btn-layout');
     const $input = $('rice-payment-input');
-    const $errorMessage = $('.error-message');
-    const numberRegex = /^[0-9]+$/;
 
-    $(document).ready(function(){
-    
-    // 버튼 클릭시 모달창 띄우기 
-    $open.click(function(){
-        $layout.show();
-    });
-    
-    // x버튼 클릭시 모달창 닫기
-    $close.click(function(){
-        $layout.hide();
-    })
-    
+
+// 버튼 클릭시 모달창 띄우기
+$open.click(function(){
+    $('.support-layout').html(
+        `
+            <div class="support-rice-layout">
+             <form name="goForm"> 
+            <div class="suppport-rice-container">
+                <div class="support-rice-close-btn-layout">
+                    <button type="button" onclick="modalClose()">X</button>
+                </div>
+                <div class="">
+                    <input type="text" placeholder="공양미 입력"  class="rice-payment-input riceInput" value="">
+                     <p class="error" style="font-size: 13px;"></p>
+                    <p>후원 가능한 공양미: <em>${memberDTO.memberRice}</em>석</p>
+                </div>
+        
+            </div>
+            <div class="support-pay-layout">
+                <button type="button" onclick="confirm()" class="btn-rice">
+                    <span>확인</span>
+                </button>
+            </div>
+             </form> 
+        </div>
+        `
+    )
+    $layout.show();
+
     // 전송
     // $submit.click(function(){
     //     document.$submit();
     // });
 
-  });
-const $resultModal = $('.support-layout-error');
-const $modalClose = $('.support-rice-close-btn-layout-error');
-const $closeButton = $('.support-pay-layout-error');
-    $submit.on('click',function(e){
-        $resultModal.show();
-        $layout.hide()
-    })
-    $modalClose.click(function (){
-        $resultModal.hide();
-        $layout.show()
-    })
-    $closeButton.click(function (){
-        $resultModal.hide();
-        $layout.show()
-    })
+})
+
+function modalClose(){
+    console.log("들어옴");
+    $layout.hide();
+}
+
+const $inputs = $('.rice-payment-input');
+const $errorMessage = $('.error-message');
+const numberRegex = /^[0-9]+$/;
+
+function confirm(){
+
+    // input값이 비어있으면 에러 메시지 출력
+    if(!$('.riceInput').val()){
+        $('.error').text('비워둘 수 없습니다.');
+    }else{
+        $('.error').hide();
+    }
+
+    if(!numberRegex.test($('.riceInput').val()) && $('.riceInput').val()) {
+        $('.error').text('숫자만 입력하세요.');
+        $('.error').show();
+    }
+
+    if($('.error').css('display') == 'block') {
+        return;
+    }
+    console.log("들어옴?");
+    // 다 통과했다면 submit
+    document.goForm.submit();
+};
+
+// 환전할 수 있는 공양미 보다 큰 값을 입력했을 경우 환전할 수 있는 최대 공양미 수로 변경
+$('.riceInput').on('blur', function() {
+    if (parseInt($(this).val()) > parseInt($('.rice-payment-input').text())) {
+        $(this).val($('.rice-payment-input').text());
+    }
+
+})
+
+
+// x버튼 클릭시 모달창 닫기
+//
+// const $resultModal = $('.support-layout-error');
+// const $modalClose = $('.support-rice-close-btn-layout-error');
+// const $closeButton = $('.support-pay-layout-error');
+//     $submit.on('click',function(e){
+//         $resultModal.show();
+//         $layout.hide()
+//     })
+//     $modalClose.click(function (){
+//         $resultModal.hide();
+//         $layout.show()
+//     })
+//     $closeButton.click(function (){
+//         $resultModal.hide();
+//         $layout.show()
+//     })
 
 
 
 
 
-
-// const $input = $('rice-payment-input');
-// const $errorMessage = $('.error-message');
-// const numberRegex = /^[0-9]+$/;
-
-// $('.change-modal-ok-btn').on('click', () => {
-//     // input값이 비어있으면 에러 메시지 출력
-//     $input.each((i, e) => {
-//         if(!$(e).val()) {
-//             $errorMessage.eq(i).text('비워둘 수 없습니다.');
-//             $errorMessage.eq(i).show();
-//         } else {
-//             $errorMessage.eq(i).hide();
-//         }
-//     });
-
-  $('.btn-rice').on('click', () => {
-      // input값이 비어있으면 에러 메시지 출력
-      $input.each((i, e) => {
-          if(!$(e).val()) {
-            console.log(i);
-            console.log(e);
-              $errorMessage.eq(i).text('비워둘 수 없습니다.');
-              $errorMessage.eq(i).show();
-          } else {
-              $errorMessage.eq(i).hide();
-          }
-      });
-
-    // 계좌번호와 환전할 수 있는 공양미는 숫자만 입력받게 정규식 테스트
-        for(let i = 1; i < $errorMessage.length; i++) {
-            if(!numberRegex.test($input.eq(i).val()) && $input.eq(i).val()) {
-                $errorMessage.eq(i).text('숫자만 입력하세요.');
-                $errorMessage.eq(i).show();
-            }
-        }
-
-    });
-
-//     // 에러 메시지가 떠있는게 있으면 빠져나가기
-//     for(let i = 0; i < $errorMessage.length; i++) {
-//         if($errorMessage.eq(i).css('display') == 'block') {
-//             return;
-//         }
-//     }
-
-//     // 다 통과했다면 submit
-//     document.ricePayed.submit();
-// });
-
-// // 환전할 수 있는 공양미 보다 큰 값을 입력했을 경우 환전할 수 있는 최대 공양미 수로 변경
-// $input.eq(2).on('blur', function() {
-//     if(parseInt($(this).val()) > parseInt($('rice-payment-input').text())) {
-//         $(this).val($('rice-payment-input').text(300));
-//     }
-// });
