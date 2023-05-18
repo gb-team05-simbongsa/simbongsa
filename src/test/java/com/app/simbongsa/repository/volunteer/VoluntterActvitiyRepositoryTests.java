4646e9354c99285b147515cbfd8fbe27f9679ef3
coder-juyeon <test.2678ju@gmail.com>
2023-05-19 03:52:50 +0900

    봉사활동 내역

--- src/test/java/com/app/simbongsa/repository/volunteer/VoluntterActvitiyRepositoryTests.java
+++ src/test/java/com/app/simbongsa/repository/volunteer/VoluntterActvitiyRepositoryTests.java
@@ -1,6 +1,10 @@
 package com.app.simbongsa.repository.volunteer;
 
+import com.app.simbongsa.entity.member.Member;
+import com.app.simbongsa.entity.volunteer.VolunteerWork;
 import com.app.simbongsa.entity.volunteer.VolunteerWorkActivity;
+import com.app.simbongsa.provider.UserDetail;
+import com.app.simbongsa.repository.member.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.Optional;
 
 @SpringBootTest
 @Transactional
@@ -18,13 +24,36 @@ import javax.transaction.Transactional;
 public class VoluntterActvitiyRepositoryTests {
     @Autowired
     private VolunteerWorkActivityRepository volunteerWorkActivityRepository;
-
+    private MemberRepository memberRepository;
+    private VolunteerWorkRepository volunteerWorkRepository;
     /* 내 봉사 활동 목록 조회(페이징처리) */
     @Test
+    public void saveTest(){
+
+        Optional<Member> memberOptional = memberRepository.findById(595L);
+        Optional<VolunteerWork> volunteerWorkOptional = volunteerWorkRepository.findById(197L);
+
+        memberOptional.ifPresent(member -> {
+            volunteerWorkOptional.ifPresent(volunteerWork -> {
+                VolunteerWorkActivity volunteerWorkActivity = new VolunteerWorkActivity(
+                        LocalDate.now(),
+                        member,
+                        volunteerWork
+                );
+                volunteerWorkActivityRepository.save(volunteerWorkActivity);
+
+                // 테스트에 필요한 동작 수행
+            });
+        });
+
+    }
+    @Test
     public void findByMemberId() {
-        PageRequest pageRequest = PageRequest.of(0,3);
-        Page<VolunteerWorkActivity> foundVolunteerWorkActivities = volunteerWorkActivityRepository.findByMemberId(pageRequest, 143L);
+        /*PageRequest pageRequest = PageRequest.of(0,3);
+        UserDetail userDetail = new UserDetail();
+        userDetail.setId(561L);
+        Page<VolunteerWorkActivity> foundVolunteerWorkActivities = volunteerWorkActivityRepository.findMyVolunteerById(pageRequest, userDetail);
         foundVolunteerWorkActivities.stream().map(VolunteerWorkActivity::toString).forEach(log::info);
-        log.info("====================유저 아이디 143의  봉사 활동 목록수=================" + foundVolunteerWorkActivities.getTotalElements());
+        log.info("====================유저 아이디 143의  봉사 활동 목록수=================" + foundVolunteerWorkActivities.getTotalElements());*/
     }
 }
