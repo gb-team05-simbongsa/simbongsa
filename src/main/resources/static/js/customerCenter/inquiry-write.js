a0de2886547397830c525869c7d1c807562edfd5
dev-hyunjin <disappointed123419@gmail.com>
2023-05-23 21:26:14 +0900

    inquiry-write 붙이는 중

--- src/main/resources/static/js/customerCenter/inquiry-write.js
+++ src/main/resources/static/js/customerCenter/inquiry-write.js
@@ -1,6 +1,6 @@
-const $close = $('.inquiry-close-btn-wrap');
-const $main = $('.main');
-const $open = $('.footer-link-customer-channel-talk');
+const $closeInquiry = $('.inquiry-close-btn-wrap');
+const $mainInquiry = $('.main');
+const $openInquiry = $('.footer-link-customer-channel-talk');
 
 var Target = document.getElementById("clock");
 var Target_apm = document.getElementById("apm");
@@ -29,15 +29,15 @@ setInterval(clock, 1000); // 1초마다 실행
 $(document).ready(function(){
     
     // 버튼 클릭시 모달창 띄우기 
-    $open.click(function(){
-        $main.show();
+    $openInquiry.click(function(){
+        $mainInquiry.show();
     });
     
     
     
     // x버튼 클릭시 모달창 닫기
-    $close.click(function(){
-      $main.hide();
+    $closeInquiry.click(function(){
+      $mainInquiry.hide();
     });
 
 });
