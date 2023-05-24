ba6ffd0548a4acbe079f80e3175c5c2ad24c6b99
LFseohyeon <m9037m@gmail.com>
2023-05-24 18:08:30 +0900

    댓글 시간 날짜 오류 수정완료

--- src/main/resources/static/js/community/free-detail.js
+++ src/main/resources/static/js/community/free-detail.js
@@ -57,6 +57,18 @@ $('.comment-box-span').on('keyup', () => {
 /*====================================================================================================================*/
 /*댓글*/
 /*====================================================================================================================*/
+function formatDate(timestamp) {
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+
 let page = 0;
 
 const $registerButton = $(".singUp");
@@ -124,12 +136,13 @@ function repliesContent(replies) {
                     <div style="flex: 0 0 auto; height: 5px;"></div>
                     <p class="comment-contant">${reply.freeBoardReplyContent}</p>
                     <div style="flex: 0 0 auto; height: 8px;"></div>
-                    <span class="comment-day">${reply.createdDate}</span>
+                    <span class="comment-day">${formatDate(reply.createdDate)}</span>
                 </div>
             </li>
         `;
     });
     return text;
+
 }
 
 // 댓글 등록 시
