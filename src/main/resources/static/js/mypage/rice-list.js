cf3bb41ed6645b76ecbd92945a26fedf94ce9fa3
coder-juyeon <test.2678ju@gmail.com>
2023-05-25 03:15:50 +0900

    마이페이지 오류 수정

--- src/main/resources/static/js/mypage/rice-list.js
+++ src/main/resources/static/js/mypage/rice-list.js
@@ -3,7 +3,6 @@ myRicePayment.forEach((myrice, i) => {
 
     text = `
         <tr>
-            <td>${myrice.memberDTO.memberRice}석</td>
             <td>${myrice.ricePaymentUsed}석</td>
             <td>${myrice.createdDate.substring(0, 10)} ${myrice.createdDate.substring(11, 19)}</td>
             <td>${myrice.ricePaymentStatus}</td>
