983cbfd01bbafc2a439066b4eb3684b9e5cae492
dev-hyunjin <disappointed123419@gmail.com>
2023-05-31 10:12:46 +0900

    오류 페이지 수정

--- src/main/java/com/app/simbongsa/controller/ErrorController.java
+++ src/main/java/com/app/simbongsa/controller/ErrorController.java
@@ -7,10 +7,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/error/*")
 @RequiredArgsConstructor
 @Slf4j
 public class ErrorController {
-    @GetMapping("error")
-    public String error() { return "error/404.html";}
+
+    @GetMapping("400")
+    public String error400() {
+        return "error/400";
+    }
+    @GetMapping("401")
+    public String error401() {
+        return "error/401";
+    }
+    @GetMapping("403")
+    public String error403() {
+        return "error/403";
+    }
+    @GetMapping("404")
+    public String error404() {
+        return "error/404";
+    }
+    @GetMapping("405")
+    public String error405() {
+        return "error/405";
+    }
+    @GetMapping("500")
+    public String error500() {
+        return "error/500";
+    }
 }
